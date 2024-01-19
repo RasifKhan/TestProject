97fa3927212dc8662ccc6faff2ca9ff5685cb018
RasifKhan <rasifkhanrasu@gmail.com>
2024-01-19 21:34:23 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i<10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            Console.WriteLine("Another Loop");
+
+            int y = 0;
+            while (y < 10)
+            {
+                y++;
+                Console.WriteLine(y);
+              
+            }
+        }
+    }
+}
